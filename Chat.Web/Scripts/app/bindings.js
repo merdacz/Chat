cd07fb95fd4d578f2213e2a4ea55ba3c54bcdba0
Marcin Daczkowski <merdacz@ais.pl>
2015-10-03 18:10:17 +0200

    refactor: cleaned up solution a bit

---
+++ Chat.Web/Scripts/app/bindings.js
@@ -0,0 +1,32 @@
+﻿ko.bindingHandlers.fadeOut = {
+    update: function (element, valueAccessor, allBindings, viewModel, bindingContext) {
+        var value = valueAccessor();
+        var valueUnwrapped = ko.unwrap(value);
+        $(element).text(valueUnwrapped).delay(3000).fadeOut();
+        if (valueUnwrapped) {
+            viewModel.error(' '); // makes next error to show up correctly even if it is the same as previous one. 
+        }
+    }
+};
+
+ko.bindingHandlers.enterKey = {
+    init: function (element, valueAccessor, allBindings, viewModel) {
+        var callback = valueAccessor();
+        $(element).keypress(function (event) {
+            var keyCode = (event.which ? event.which : event.keyCode);
+            if (keyCode === 13) {
+                callback.call(viewModel);
+                return false;
+            }
+            return true;
+        });
+    }
+};
+
+ko.bindingHandlers.hidden = {
+    update: function (element, valueAccessor) {
+        ko.bindingHandlers.visible.update(element, function () {
+            return !ko.utils.unwrapObservable(valueAccessor());
+        });
+    }
+};
---
+++ Chat.Web/Scripts/app/views.js
@@ -0,0 +1,106 @@
+﻿function messageViewModel(username, text) {
+    var self = this;
+
+    self.username = ko.observable(username);
+    self.text = ko.observable(text);
+}
+
+function userViewModel(username) {
+    var self = this;
+
+    self.username = ko.observable(username);
+}
+
+function chatViewModel() {
+    var self = this;
+
+    self.hub = $.connection.chatHub;
+    self.error = ko.observable('');
+    self.username = ko.observable().extend({ required: true, minLength: 5 });
+    self.joined = ko.observable(false);
+    self.newMessageText = ko.observable().extend({ required: true });
+    self.users = ko.observableArray();
+    self.messages = ko.observableArray();
+
+    self.userScrolled = false;
+    self.autoScrolling = false;
+
+    self.hub.client.invalidOperation = function (errorMessage) {
+        self.error(errorMessage);
+    };
+
+    self.hub.client.joinedSuccessfully = function (userList, recentMessages) {
+        var mappedUsers = $.map(userList, function (usr) {
+            return new userViewModel(usr);
+        });
+        self.users(mappedUsers);
+
+        var mappedMessages = $.map(recentMessages, function (msg) {
+            return new messageViewModel(msg.UserName, msg.Text);
+        });
+        self.messages(mappedMessages);
+        self.joined(true);
+        $("#msg").focus();
+    }
+
+    self.hub.client.leftSuccessfully = function () {
+        self.joined(false);
+    }
+
+    self.hub.client.newUserJoined = function (username) {
+        self.users.push(new userViewModel(username));
+    };
+
+    self.hub.client.userLeft = function (username) {
+        self.users.remove(function (item) {
+            return item.username() === username;
+        });
+    };
+
+    self.hub.client.newMessage = function (username, message) {
+        self.messages.push(new messageViewModel(username, message));
+        self.adjustScroll();
+    };
+
+
+    self.sendMessage = function () {
+        if (!self.newMessageText()) {
+            return;
+        }
+
+        self.hub.server.sendMessage(this.newMessageText());
+        self.newMessageText("");
+    };
+
+    self.join = function () {
+        self.messages.removeAll();
+        self.users.removeAll();
+        self.hub.server.join(self.username());
+    };
+
+    self.leave = function () {
+        self.hub.server.leave();
+    };
+
+    self.adjustScroll = function () {
+        if (!self.userScrolled) {
+            self.autoScrolling = true;
+            var container = $("#messagesContainer");
+            container.animate({ scrollTop: container.prop("scrollHeight") }, 500);
+            self.autoScrolling = false;
+        }
+    };
+
+    self.handleUserScroll = function () {
+        if (self.autoScrolling) {
+            return;
+        }
+
+        var container = $("#messagesContainer");
+        if (container.scrollTop() + container.innerHeight() >= container.prop("scrollHeight")) {
+            self.userScrolled = false;
+        } else {
+            self.userScrolled = true;
+        }
+    };
+};
